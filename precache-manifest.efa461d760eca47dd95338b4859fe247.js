bed9e77449ab0a3373c3e6eab6168bfaac5d7572
amirdbt <hassanamir643@yahoo.com>
2019-08-30 04:48:49 -0700

    Updates

---
+++ precache-manifest.efa461d760eca47dd95338b4859fe247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da38cacaf556f8aa32122298b1aef63f",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "a0881ddc0cb3ea2e66b5",
+    "url": "/memegenerator/static/css/main.31a0fc4a.chunk.css"
+  },
+  {
+    "revision": "74d329a98022c85ea357",
+    "url": "/memegenerator/static/js/2.0251a58e.chunk.js"
+  },
+  {
+    "revision": "a0881ddc0cb3ea2e66b5",
+    "url": "/memegenerator/static/js/main.18263d18.chunk.js"
+  },
+  {
+    "revision": "393d2b88a6c217d30d5f",
+    "url": "/memegenerator/static/js/runtime~main.1311c363.js"
+  }
+]);
